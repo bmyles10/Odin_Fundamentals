99b0514e6aab27d6b24faa2a53f02bea01bcce8f
Brandon Myles <bmyles1006@gmail.com>
2019-01-31 15:17:06 -0600

    Added Assignment file

---
+++ Assignments/assignment1.js
@@ -0,0 +1,14 @@
+
+// 4 variables: first name, last name, current year, and birth year.
+// a 5th variable (greeting) that is constructed from the previous 4, it should contain a greeting with the person's full name and their age.
+// print the greeting with console.log
+let birthYear = 1948
+let thisYear = 1965
+let firstName = "Carlos"
+let lastName = "Stevenson"
+
+let greeting = 'Hello! My name is ' + firstName + + lastName + ' and I am ' + (thisYear - birthYear) + " years old.";
+
+console.log(greeting);
+//Hello! My name is Carlos Stevenson and I am 17 years old.
+console.log(50/13);
